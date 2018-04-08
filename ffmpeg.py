0985cbfbc34fdfce7ea5638f16cf3e911e5f293f
Dhia Ariq Muhammad <dhiaariq@sisalma.com>
2018-04-08 17:09:20 +0700

    l

--- ffmpeg.py
+++ ffmpeg.py
@@ -3,8 +3,8 @@ import time
 import subprocess
 
 def ffmpeg_call(input,name):
-    subprocess.call(['ffmpeg -i '+input+' -f segment -vcodec copy -an -reset_timestamps 1 -map 0 /mnt/volume-sgp1-01/proj/'+name+'/OUTPUT%d_Orig.mp4'])
-    subprocess.call(['ffmpeg -i '+input+' -vn -acodec libopus /mnt/volume-sgp1-01/proj/'+name+'/OUTPUT.opus'])
+    subprocess.call(['ffmpeg','-i '+input+' -f segment -vcodec copy -an -reset_timestamps 1 -map 0 /mnt/volume-sgp1-01/proj/'+name+'/OUTPUT%d_Orig.mp4'])
+    subprocess.call(['ffmpeg','-i '+input+' -vn -acodec libopus /mnt/volume-sgp1-01/proj/'+name+'/OUTPUT.opus'])
 
 def create_folder(path,name):
     os.makedirs(str(path)+str(name))
