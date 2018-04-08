017da259bba9dfeef22d8a9c6802ab8aa06f7383
Dhia Ariq Muhammad <dhiaariq@sisalma.com>
2018-04-08 11:57:04 +0700

    Delet main

--- ffmpeg.py
+++ ffmpeg.py
@@ -3,11 +3,11 @@ import time
 import subprocess
 
 def ffmpeg_call(name):
-    subprocess.call(['ffmpeg','-i',name,'*','-f segment -vcodec copy -an -reset_timestamps 1 -map 0 u/',name,'/OUTPUT%d_Orig.mp4'])
-    subprocess.call(['ffmpeg','-i',name,'*','-vn -acodec aac u/',name,'/OUTPUT.aac'])
+    subprocess.call(['ffmpeg','-i',name,'*','-f segment -vcodec copy -an -reset_timestamps 1 -map 0 /mnt/volume-sgp1-01/proj/',name,'/OUTPUT%d_Orig.mp4'])
+    subprocess.call(['ffmpeg','-i',name,'*','-vn -acodec aac /mnt/volume-sgp1-01/proj/',name,'/OUTPUT.aac'])
 
 def create_folder(name):
-    os.makedirs("u/"+str(name))
+    os.makedirs("/mnt/volume-sgp1-01/"+str(name))
 
 def read_file_in_folder(folder_name):
     pass
