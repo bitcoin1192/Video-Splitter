c40446173602945424f85ad117621f1567988c52
Dhia Ariq Muhammad <dhiaariq@sisalma.com>
2018-04-21 06:44:39 +0700

    jj

--- Client/client.py
+++ Client/client.py
@@ -1,54 +1,47 @@
-#import requests
 import http.client
-#import http
 import re
 import time
 import json
 import codecs
+import subprocess
+import os
 
-conn = http.client.HTTPConnection("localhost",port=8888)
-#conn1 = requests.get("http://localhost:8888/create")
-def start():
-    print('ready for testing')
-    print('press enter to start')
-    return main()
+conn = http.client.HTTPConnection('api.sisalma.com',port=80)
+conn1 = http.client.HTTPConnection('cdn.sisalma.com',port=80)
+
+
+global id
+global part
+
+def download(file,path):
+    link = "/"+path+'/'+file
+    lls = conn.request("GET", link, headers={'Content-type':'application/json'})
+    if lls is None:
+        return False
+    return True
+
+def upload(file,path):
+    link = "/"+path+'/'+file
+    with open('/'+file, 'rb') as byte:
+        lls = conn.request("PUT", link, body=byte, headers={'Content-type':'octet-stream/webm'})
+    return True
+
+def something():
+    link = conn.request("GET", "/slave", headers={'Content-type':'application/json'})
+    jobid = link.getresponse()
+    result = json.load(jobid)
+    if result is None:# after 1 fail attempt shutdown instances immediately
+        return None, None
+    id = result['id']
+    part = result['part']
+    return id, part
+    
+def ffmpeg_call(input):
+    subprocess.call(['ffmpeg','-i',input,'-f','segment','-vcodec','copy','OUTPUT%d_Orig.webm'])
 
 def main():
-    p = re.compile("/'(.*?)'/s")
-    counter = 0
-    step = 0
-    state = False
-    array = []
-    while state is False:
-        if step == 0:#create job
-            conn.request("GET", "/create", headers={'Content-type':'application/json'})
-            jobid = conn.getresponse()
-            ww = p.match(str(jobid.read()))
-            ss = json.loads(codecs.open(ww,mode="r",encoding="utf-8"))
-            array.append(ss)
-            print(array)
-            if jobid.status == 500:
-                print("error while creating job")
-                time.sleep(10000)
-            if counter == 100:
-                step = 1
-                counter = 0
-                print("succesfully create jobid")
-            counter += 1
-        elif step == 1:#verify status
-            for a in array:
-                conn.request("GET", "/status?jobid="+str(a))
-                content = conn.getresponse()
-                if content == "wot":
-                    print("jobid not being inserted correctly")
-                    time.sleep(10000)
-            state = True
-            #time.sleep(10000)
-    print("Test Success")
-    time.sleep(10000)
-
-start()
-        #elif step == 2:#start
-        #    start = conn.request("GET", "/start?jobid="+)
-        #    if counter == 1200:
-        #        state = True
+    id, part = something()
+    if id is None:
+        os.system('echo 1 > /proc/sys/kernel/sysrq && echo b > /proc/sysrq-trigger')
+    ffmpeg_call(id+'mp4')
+    upload(id,part)
