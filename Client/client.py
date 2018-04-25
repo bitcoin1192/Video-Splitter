506bfed5d5242e65981ea65ce414f9077ab48d24
Dhia Ariq Muhammad <dhiaariq@sisalma.com>
2018-04-25 18:08:49 +0700

    kka

--- Client/client.py
+++ Client/client.py
@@ -5,13 +5,14 @@ import json
 import codecs
 import subprocess
 import os
+import requests
 
 global id
 global part
 
 def download(file,path):
     link = "/"+path+'/'+file
-    lls = conn.request("GET", link, headers={'Content-type':'application/json'})
+    lls = cdn.request("GET", link, headers={'Content-type':'application/json'})
     if lls is None:
         return False
     return True
@@ -19,11 +20,11 @@ def download(file,path):
 def upload(file,path):
     link = "/"+path+'/'+file
     with open('/'+file, 'rb') as byte:
-        lls = conn.request("PUT", link, body=byte, headers={'Content-type':'octet-stream/webm'})
+        lls = cdn.request("PUT", link, body=byte, headers={'Content-type':'octet-stream/webm'})
     return True
 
 def something():
-    link = conn.request("GET", "/slave", headers={'Content-type':'application/json'})
+    link = api.request("GET", "/slave", headers={'Content-type':'application/json'})
     jobid = link.getresponse()
     result = json.load(jobid)
     if result is None:# after 1 fail attempt shutdown instances immediately
@@ -36,12 +37,13 @@ def ffmpeg_call(input):
     subprocess.call(['ffmpeg','-i',input,'-f','segment','-vcodec','copy','OUTPUT%d_Orig.webm'])
 
 def main():
-    global conn
-    global conn1
+    global api
+    global cdn
     try:
-        conn = http.client.HTTPConnection('api.sisalma.com',port=80)
-        conn1 = http.client.HTTPConnection('cdn.sisalma.com',port=80)
-    except TimeoutError:
+        api = requests.get('api.sisalma.com')
+        cdn = requests.get('cdn.sisalma.com')
+        api.raise_for_status()
+    except (requests.exceptions.ConnectTimeout, requests.exceptions.HTTPError):
         print('error handled')
         os.system('echo 1 > /proc/sys/kernel/sysrq && echo b > /proc/sysrq-trigger')
     id, part = something()
