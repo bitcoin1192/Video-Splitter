439da2c1ec85dbc36ed12b25dfe2fc412a24cd89
Muhammad Dhia Ariq <dhiaariq@live.com>
2018-05-07 17:40:44 +0700

    kkk

---
+++ Client/ffmpeg.py
@@ -0,0 +1,16 @@
+import os
+import time
+import subprocess
+
+#Extract Video and subtitle and convert Audio to opus format
+def ffmpeg_call(input,name):
+    subprocess.call(['ffmpeg','-i',input,'-f','segment','-vcodec','copy','-an','-reset_timestamps','1','-map','0', name+'/OUTPUT%d_Orig.mp4'],shell=False)
+    subprocess.call(['ffmpeg','-i',input,'-vn','-acodec', 'libopus', name+'/OUTPUT.opus'],shell=False)
+    subprocess.call(['ffmpeg','-i',input,'-map','0:s:0', name +'/OUTPUT.ass'],shell=False)
+
+#Create folder path for project
+def create_folder(path,name):
+    os.makedirs(str(path)+str(name))
+
+def read_file_in_folder(folder_name):
+    pass
