138068fec07c07f5d4db6272c4d548505a95b6dc
Muhammad Dhia Ariq <dhiaariq@live.com>
2018-05-08 16:55:01 +0700

    eqwe

--- tornapart-edit.py
+++ tornapart-edit.py
@@ -61,7 +61,7 @@ class slave_comm(tornado.web.RequestHandler):
             content = json.dumps({'job' : job, 'part' : part}, separators=(',', ':'))
             self.write(content)
             self.finish()
-        except:
+        except(IndexError):
             self.write_error(404)
 
 def main():
