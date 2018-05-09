f8337e80d7fb3781806a9a7eaeaf9f9d060fe639
Muhammad Dhia Ariq <dhiaariq@live.com>
2018-05-09 22:32:27 +0700

    we

--- Client/client.py
+++ Client/client.py
@@ -23,8 +23,8 @@ def download(job):
 def upload(i):
     #input, name = i[0], i[1]
     out = os.path.splitext(i[1])[0]
-    multifiles = [{'video', (out+'.webm', open('encode/'+i[0]+'/'+out+'.webm', mode='rb')), 'video/webm'}]
-    requests.post('http://api.sisalma.com/upload?proj_id='+i[0],files = multifiles, timeout=10)
+    files = {'file': (out+'.webm', open('encode/'+i[0]+'/'+out+'.webm', mode='rb'), 'video/webm')}
+    requests.post('http://api.sisalma.com/upload?proj_id='+i[0],files = files, timeout=10)
     return True
 
 def get_job(cpu_c):
