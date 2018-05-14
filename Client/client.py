3377bfa1e7615c220ea58c38af9394caa08ed9e2
Muhammad Dhia Ariq <dhiaariq@live.com>
2018-05-15 06:25:37 +0700

    jhghjg

--- Client/client.py
+++ Client/client.py
@@ -63,6 +63,7 @@ def ffmpeg_call(i):
     input, name = i[0], i[1]
     out = os.path.splitext(name)[0]
     subprocess.run(['ffmpeg','-i','proj/'+input+'/'+out+'.mp4','-minrate','300k','encode/'+input+'/'+out+'.webm'])
+    return True
 
 def exit_gracefully():
     hostname = platform.node()
