513c5c8ee69eaaaba2b6cd5df8df00b64e71ff93
Muhammad Dhia Ariq <dhiaariq@live.com>
2018-05-15 06:09:33 +0700

    dasd

--- fileiterator.py
+++ fileiterator.py
@@ -2,7 +2,7 @@ import re
 import os
 
 def listfilebyformats(directory,format):
-	file = open('index','w')
+	file = open(directory+'index','w')
 	convert = lambda text: int(text) if text.isdigit() else text
 	alphanum_key = lambda key: [ convert(c) for c in re.split('([0-9]+)', key) ]
 	lists = sorted(os.listdir(directory), key=alphanum_key)
