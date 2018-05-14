47f864b4d6af8ee9b7e2ce585020723d21f91c0f
Muhammad Dhia Ariq <dhiaariq@live.com>
2018-05-15 06:31:41 +0700

    sadsaddasd

--- Client/client.py
+++ Client/client.py
@@ -63,7 +63,7 @@ def ffmpeg_call(i):
     input, name = i[0], i[1]
     print(i)
     out = os.path.splitext(name)[0]
-    subprocess.run(['ffmpeg','-i','proj/'+input+'/'+out+'.mp4','encode/'+input+'/'+out+'.webm'])
+    subprocess.run(['ffmpeg','-i','proj/'+input+'/'+out+'.mp4','encode/'+input+'/'+out+'.webm','-loglevel','quiet'])
     return True
 
 def exit_gracefully():
