e162988e8397fd094597e2795567daeca2787745
Muhammad Dhia Ariq <dhiaariq@live.com>
2018-05-14 15:39:24 +0700

    kjlkj

--- ffmpeg.py
+++ ffmpeg.py
@@ -4,9 +4,9 @@ import subprocess
 
 #Extract Video and subtitle and convert Audio to opus format
 def ffmpeg_call(input,name):
-    subprocess.run(['ffmpeg','-i',input,'-f','segment','-vcodec','copy','-an','-reset_timestamps','1','-map','0', name+'/OUTPUT%d_Orig.mp4'],shell=False)
-    subprocess.run(['ffmpeg','-i',input,'-vn','-acodec', 'libopus', name+'/OUTPUT.opus'],shell=False)
-    subprocess.run(['ffmpeg','-i',input,'-map','0:s:0', name +'/OUTPUT.ass'],shell=False)
+    subprocess.run(['ffmpeg','-i',input,'-f','segment','-vcodec','copy','-an','-reset_timestamps','1','-map','0', name+'/OUTPUT%d_Orig.mp4','-loglevel','quiet'],shell=False)
+    subprocess.run(['ffmpeg','-i',input,'-vn','-acodec', 'libopus', name+'/OUTPUT.opus','-loglevel','quiet'],shell=False)
+    subprocess.run(['ffmpeg','-i',input,'-map','0:s:0', name +'/OUTPUT.ass','-loglevel','quiet'],shell=False)
 
 #Create folder path for project
 def create_folder(path,name):
