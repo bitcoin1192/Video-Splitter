1dd053943580b709c8f8f8c4c07d388ad8e4fe70
Muhammad Dhia Ariq <dhiaariq@live.com>
2018-05-17 15:44:27 +0700

    dkasl

--- ffmpeg.py
+++ ffmpeg.py
@@ -5,6 +5,8 @@ import subprocess
 #Extract Video and subtitle and convert Audio to opus format
 def ffmpeg_call(input,name):
     subprocess.run(['ffmpeg','-i',input,'-f','segment','-vcodec','copy','-an','-reset_timestamps','1','-map','0', name+'/OUTPUT%d_Orig.mp4','-loglevel','quiet'],shell=False)
+
+def ffmpeg_audio(input,name):
     subprocess.run(['ffmpeg','-i',input,'-vn','-acodec', 'libopus', name+'/OUTPUT.opus','-loglevel','quiet'],shell=False)
     subprocess.run(['ffmpeg','-i',input,'-map','0:s:0', name +'/OUTPUT.ass','-loglevel','quiet'],shell=False)
 
