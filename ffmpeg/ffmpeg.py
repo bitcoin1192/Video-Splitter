9d7b875bf932e4cdfc4cc477c0ca6e35beb35b69
Muhammad Dhia Ariq <dhiaariq@live.com>
2018-05-20 21:24:06 +0700

    trying to fix ffmpeg cmd

--- ffmpeg/ffmpeg.py
+++ ffmpeg/ffmpeg.py
@@ -5,11 +5,11 @@ import subprocess
 #Extract Video and subtitle and convert Audio to opus format
 #'-loglevel','quiet'
 def ffmpeg_call(input,name):
-    subprocess.run(['ffmpeg','-i',input,'-f','segment','-vcodec','copy','-an','-sn','-reset_timestamps','1','-map','0', name+'/OUTPUT%d_Orig.mp4'],shell=False)
+    subprocess.run(['ffmpeg','-i',input,'-f','segment','-vcodec','copy','-an','-sn','-reset_timestamps','1',name+'/OUTPUT%d_Orig.mp4'],shell=False)
 
 def ffmpeg_audio(input,name):
     subprocess.run(['ffmpeg','-i',input,'-vn','-sn','-acodec', 'libopus', name+'/OUTPUT.opus','-loglevel','quiet'],shell=False)
-    #subprocess.run(['ffmpeg','-i',input,'-map','0:s:0', name +'/OUTPUT.ass','-loglevel','quiet'],shell=False)
+    subprocess.run(['ffmpeg','-i',input,'-vn','-an', name +'/OUTPUT.ass','-loglevel','quiet'],shell=False)
 
 #Create folder path for project
 def create_folder(path,name):
