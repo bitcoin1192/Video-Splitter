c3dd4c8b969ac13f199c3f05c09657963a942820
Muhammad Dhia Ariq <dhiaariq@live.com>
2018-05-27 05:58:09 +0700

    dasd

--- client.py
+++ client.py
@@ -61,6 +61,7 @@ def get_job(cpu_c):
         except(requests.exceptions.ConnectTimeout, requests.exceptions.HTTPError):    
             return list_job
         dict_responses = r.json()
+        print (dict_responses)
         list_job.append(list(dict_responses.values()))
         count = count + 1
     if not list_job:
