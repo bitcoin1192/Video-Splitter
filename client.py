6427c214b0117638b7aa3796239e0a5a15b13f41
Muhammad Dhia Ariq <dhiaariq@live.com>
2018-05-29 13:04:50 +0700

    spsp

--- client.py
+++ client.py
@@ -76,7 +76,7 @@ def get_job(cpu_c):
 def ffmpeg_call(i):
     input, name = i[0], i[1]
     out = os.path.splitext(name)[0]
-    subprocess.run(['ffmpeg','-i','proj/'+input+'/'+out+'.mp4','-crf','25','-b:v','1000k','-minrate','500k','-maxrate','1500k','encode/'+input+'/'+out+'.webm','-loglevel','quiet'])
+    subprocess.run(['ffmpeg','-i','proj/'+input+'/'+out+'.mp4','-crf','23','-b:v','1200k','-minrate','500k','-maxrate','1700k','encode/'+input+'/'+out+'.webm','-loglevel','quiet'])
     
     return True
 
