58eeefe3cf5a37246f81378761bd5d3c84d3f829
Muhammad Dhia Ariq <dhiaariq@live.com>
2018-06-02 05:30:35 +0700

    dasd

--- ffmpeg/ffmpeg.py
+++ ffmpeg/ffmpeg.py
@@ -8,7 +8,7 @@ def ffmpeg_call(input,name):
     subprocess.run(['ffmpeg','-i',input,'-f','segment','-c','copy','-sn','-reset_timestamps','1',name+'/OUTPUT%d_Orig.mp4'],shell=False)
 
 def ffmpeg_audio(input,name):
-    #subprocess.run(['ffmpeg','-i',input,'-vn','-sn','-acodec', 'libopus', name+'/OUTPUT.opus','-loglevel','quiet'],shell=False)
+    subprocess.run(['ffmpeg','-i',input,'-vn','-sn','-acodec', 'libopus', name+'/OUTPUT.opus','-loglevel','quiet'],shell=False)
     subprocess.run(['ffmpeg','-i',input,'-vn','-an', name +'/OUTPUT.ass','-loglevel','quiet'],shell=False)
 
 #Create folder path for project
@@ -19,8 +19,8 @@ def create_folder(path,name):
         return
 
 def ff_stitch(name):
-    subprocess.run(['ffmpeg','-f','concat','-safe','0','-i',name+'index','-c','copy','/mnt/volume-sgp1-01/cdn/test.webm'],shell=False)
-    #subprocess.run(['ffmpeg','-y','-i',name+'output.webm','-i',name+'OUTPUT.opus','-c:v','copy','-c:a','copy','/mnt/volume-sgp1-01/cdn/test.webm'],shell=False)
+    subprocess.run(['ffmpeg','-f','concat','-safe','0','-i',name+'index','-c','copy',name+'output.webm'],shell=False)
+    subprocess.run(['ffmpeg','-y','-i',name+'output.webm','-i',name+'OUTPUT.opus','-c:v','copy','-c:a','copy','/mnt/volume-sgp1-01/cdn/test.webm'],shell=False)
 
 
 #ffmpeg -f concat -safe 0 -i mylist.txt -c copy output
